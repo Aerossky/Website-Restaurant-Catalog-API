e6239ceab4d3d1ec060c955206305639ec7bdce6
Aerossky <92148435+Aerossky@users.noreply.github.com>
2024-05-12 17:02:42 +0700

    Drawer - fix bug

--- src/scripts/utils/drawer-initiator.js
+++ src/scripts/utils/drawer-initiator.js
@@ -15,17 +15,23 @@ const DrawerInitiator = {
 
     _toggleDrawer(event, drawer) {
         event.stopPropagation();
-        if (drawer.style.display === 'block') {
-            drawer.style.display = 'none';
-        } else {
-            drawer.style.display = 'block';
+        const isMobile = window.innerWidth <= 768; // Menentukan apakah perangkat dianggap sebagai perangkat mobile
+        if (isMobile) {
+            if (drawer.style.display === 'block') {
+                drawer.style.display = 'none';
+            } else {
+                drawer.style.display = 'block';
+            }
         }
     },
 
     _closeDrawer(event, drawer) {
         event.stopPropagation();
         // drawer.classList.remove('open');
-        drawer.style.display = 'none';
+        const isMobile = window.innerWidth <= 768; // Menentukan apakah perangkat dianggap sebagai perangkat mobile
+        if (isMobile) {
+            drawer.style.display = 'none';
+        }
     },
 };
 
