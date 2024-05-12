97aefcdfaeb066e884d719c0b70e60d63c08798f
Aerossky <92148435+Aerossky@users.noreply.github.com>
2024-05-12 16:10:47 +0700

    API - Melakukan konfigurasi API awal dan membuat api-endpoint

---
+++ src/scripts/globals/config.js
@@ -0,0 +1,7 @@
+const CONFIG = {
+    BASE_URL: 'https://restaurant-api.dicoding.dev',
+    BASE_IMAGE_URL: 'https://restaurant-api.dicoding.dev/images/small/<pictureId>',
+    DEFAULT_LANGUAGE: 'en-us',
+  };
+   
+  export default CONFIG;
