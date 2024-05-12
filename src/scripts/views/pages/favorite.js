74c8519dc856f93d11997098309ff540f1a896d6
Aerossky <92148435+Aerossky@users.noreply.github.com>
2024-05-12 22:53:35 +0700

    Halaman - Favorite
    sudah terbuat namun belum terintegrasi dengan API

--- src/scripts/views/pages/favorite.js
+++ src/scripts/views/pages/favorite.js
@@ -1,7 +1,23 @@
 const Favorite = {
     async render() {
         return `
+        <section id="explore">
+        <div class="container">
         <h2>Favorite Page</h2>
+            <div class="explore-content">
+
+            <div class="explore-item">
+                <div class="rating">4.6</div>
+                <img src="https://restaurant-api.dicoding.dev/images/medium/41" />
+                <div class="card">
+                <p class="city">City Name</p>
+                <h3 class="name">name</h3>
+                <p class="description">Lorem ipsum dolor, sit amet consectetur adipisicing elit. Magni, tempore.</p>
+                </div>
+            </div> 
+
+            </div>
+        </section>
       `;
     },
 
