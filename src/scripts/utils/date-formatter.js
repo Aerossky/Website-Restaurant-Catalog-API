c1016120e949a502969790da92eac065219d7ae3
Aerossky <92148435+Aerossky@users.noreply.github.com>
2024-05-15 11:26:06 +0700

    Utils - Date Format
    menambahkan util yang berguna untuk melakukan format =
    tanggal bulan tahun

---
+++ src/scripts/utils/date-formatter.js
@@ -0,0 +1,10 @@
+function FormatDate(date) {
+    const options = {
+        day: 'numeric',
+        month: 'long',
+        year: 'numeric'
+    };
+    return date.toLocaleDateString('id-ID', options); // Set the locale to Indonesian ('id-ID')
+}
+
+export default FormatDate;
